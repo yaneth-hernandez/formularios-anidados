ecda501852bd0ba17769a24abaf67962fc48c9a2
Amappola <anam.porrasp@gmail.com>
2024-01-16 10:26:46 -0500

    Developing logic to select states and cities

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,18 +13,21 @@ import {
 })
 export class AppComponent implements OnInit {
   title = 'formularios-anidados';
-
   formulario!: FormGroup;
+  paisSeleccionado: string = '';
+  estadoSeleccionado: string = '';
 
   paises: string[] = ['Colombia', 'Chile', 'Brasil'];
 
   estados: Record<string,string[]> = {
+    '': [],
     'Colombia': ['Cundinamarca', 'Antioquia'],
     'Chile':['Santiago','Valparaiso'],
     'Brasil':['São Paulo', 'Rio de Janeiro']
   };
 
   ciudades: Record<string, Record<string,string[]>>  = {
+    '': {'': []},
     'Colombia':{
       'Cundinamarca':['Bogotá', 'Soacha', 'Chía', 'Funza'],
       'Antioquia':['Medellin','Rio Negro', 'Bello'],
@@ -43,21 +46,28 @@ export class AppComponent implements OnInit {
     this.formulario = this.fb.group({
       pais: ['', [Validators.required]],
       estado: ['', [Validators.required]],
-      ciudad: ['', [Validators.required]],
+      ciudad: ['', [Validators.required]]
     });
   }
 
   ngOnInit(): void {
-    this.formulario.get('pais')?.valueChanges.subscribe((pais) => {
-      this.estados = this.obtenerEstadosPorPais(pais)
-      this.formulario.get('estado')?.setValue('')
-      this.formulario.get('ciudad')?.setValue('')
-    });
+    // this.formulario.get('pais')?.valueChanges.subscribe((pais) => {
+    //   this.estados = this.obtenerEstadosPorPais(pais)
+    //   this.formulario.get('estado')?.setValue('')
+    //   this.formulario.get('ciudad')?.setValue('')
+    // });
 
-    this.formulario.get('estado')?.valueChanges.subscribe((estado) => {
-      this.estados = this.obtenerCiudadesPorEstado(estado)
-      this.formulario.get('ciudad')?.setValue('')
-    });
+    // this.formulario.get('estado')?.valueChanges.subscribe((estado) => {
+    //   this.estados = this.obtenerCiudadesPorEstado(estado)
+    //   this.formulario.get('ciudad')?.setValue('')
+    // });
+    this.formulario.get('estado')?.disable();
+    this.formulario.get('ciudad')?.disable();
+    this.formulario.get('pais')?.valueChanges
+    .subscribe((pais) => {this.paisSeleccionado = pais; this.formulario.get('estado')?.enable(), this.formulario.get('ciudad')?.disable();});
+
+    this.formulario.get('estado')?.valueChanges
+    .subscribe((estado) => {this.estadoSeleccionado = estado; this.formulario.get('ciudad')?.enable()});
   }
 
   obtenerEstadosPorPais(pais:string):string[]{
@@ -69,7 +79,12 @@ export class AppComponent implements OnInit {
   }
 
   enviarFormulario(){
-    console.log('Formulario enviado: ', this.formulario.value)
+    console.log('Formulario enviado: ', this.formulario.value);
+    this.formulario.reset();
+    this.formulario.get('estado')?.disable();
+    this.formulario.get('ciudad')?.disable();
+    this.paisSeleccionado = '';
+    this.estadoSeleccionado = '';
   }
 }
   //gestion de pasos: gestion de como los usuarios ingresan los datos
